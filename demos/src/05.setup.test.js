faca2b1cd0a32738d3a4532a5bf19dd6b23de492
Daniel Alexander Muelas Rivera <danmcode@gmail.com>
2025-09-03 16:26:39 -0500

    adding: before all

--- demos/src/05.setup.test.js
+++ demos/src/05.setup.test.js
@@ -1,7 +1,12 @@
 describe('set', () => {
+  beforeAll(() => {
+    console.log('beforeAll');
+  });
+
   test('case 1', () => {
     expect(new Set([1, 2, 3]).has(2)).toBe(true);
   });
+
   test('case 2', () => {
     expect(1 + 3).toBe(4);
   });
