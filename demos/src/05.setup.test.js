a0154e2f189c92ecb33e95398adf3d4016c5e7c4
Daniel Alexander Muelas Rivera <danmcode@gmail.com>
2025-09-04 14:12:53 -0500

    feat: knowing setup / teardown

--- demos/src/05.setup.test.js
+++ demos/src/05.setup.test.js
@@ -1,6 +1,20 @@
 describe('set', () => {
   beforeAll(() => {
-    console.log('beforeAll');
+    console.log('Open database connection');
+    // e.g initialize db connection
+  });
+
+  afterAll(() => {
+    console.log('Close database connection');
+    // e.g close db connection
+  });
+
+  beforeEach(() => {
+    console.log('execute before each test case');
+  });
+
+  afterEach(() => {
+    console.log('execute after each test case');
   });
 
   test('case 1', () => {
@@ -9,5 +23,23 @@ describe('set', () => {
 
   test('case 2', () => {
     expect(1 + 3).toBe(4);
+  });
+
+  describe('other group', () => {
+    beforeAll(() => {
+      console.log('execute before all test cases in other group');
+    });
+
+    afterAll(() => {
+      console.log('execute after all test cases in other group');
+    });
+
+    test('case 1', () => {
+      expect(new Set([1, 2, 3]).has(2)).toBe(true);
+    });
+
+    test('case 2', () => {
+      expect(1 + 3).toBe(4);
+    });
   });
 });
